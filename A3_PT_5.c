558e0273a7e4ee3038ea006d2d25742ce08e93a6
itztaylorau <itztaylorau@gmail.com>
2020-09-18 15:09:15 +0800

    Initial commit

---
+++ A3_PT_5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    /* declare constant */
+    int alpha, beta, charlie;
+    float root1, root2;
+
+    /* asking user input value */
+    printf("A:");
+    scanf("%d", &alpha);
+    printf("B:");
+    scanf("%d", &beta);
+    printf("C:");
+    scanf("%d", &charlie);
+
+    /* calculating part */
+    /* calculating root1 */
+    root1 = (-beta + (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
+    /* calculating root2 */
+    root2 = (-beta - (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
+
+    /* showing calculated result to client */
+    printf("Root 1 = %f", root1);
+    printf("Root 2 = %f", root2);
+
+    return 0;
+}
