10658123eb0a5936634d003d14862466851d9e42
itztaylorau <itztaylorau@gmail.com>
2020-09-18 16:40:51 +0800

    A3_PT_4
    A3_PT_4

---
+++ A3/PT4/A3_PT_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* declare constant */
+    int i;
+    int num; int sqr; int cub;
+
+    /* first row */
+    printf("number     square      cube\n");
+    /* loop row */
+    for(i = 1; i < 6; ++i) {
+        num = i;
+        sqr = pow(i, 2);
+        cub = pow(i, 3);
+        printf("     %d        %3.d     %5.d\n", num, sqr, cub);
+    }
+
+    return 0;
+}
