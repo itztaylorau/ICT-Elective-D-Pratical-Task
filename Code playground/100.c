47c194a1d519f33d4c2697693cdbe9d804b4ea4b
itztaylorau <itztaylorau@gmail.com>
2020-10-08 08:38:34 +0800

    Classwork Update

---
+++ Code playground/100.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int sum, i, ti;
+
+    for(i = 1; i <= 100 ; i++){
+
+        ti = i * i;
+        sum = sum + ti;
+
+
+    }
+    printf("%d", sum);
+}
