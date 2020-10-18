b804a8d777625e11253e63c1ba529c22491539da
itztaylorau <itztaylorau@gmail.com>
2020-10-18 16:43:48 +0800

    Small fix

--- A5/PT3/A5_PT_3.c
+++ A5/PT3/A5_PT_3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main() {
+
     /* Declare constant */
     int row, column;
 
-    for (row = 9; row >= 1; row--) {
+    for (row = 9; row >= 1; row--) {S
         for (column = 1; column <= row; column++) {
             printf("%d",column);
         }
