975837ab44a1c5e9976aa89061fa0e495794ee7f
itztaylorau <itztaylorau@gmail.com>
2020-10-27 19:42:22 +0800

    Added some test usage code

--- Code playground/temp.c
+++ Code playground/temp.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 int main() {
 	
-	printf("%f", sqrt(16));
+	char first_char, second_char;
+
+	printf("Please input the first character: ");
+	scanf("%c", &first_char);
+	printf("%c", first_char);
+
+	printf("Please input the second character: ");
+	scanf(" %c", &second_char);
+	printf("%c", second_char);
 
 	return 0;
 }
