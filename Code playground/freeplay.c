76045c69ba6b54d576b58d447a3d1d855b342f75
itztaylorau <itztaylorau@gmail.com>
2020-11-22 17:34:02 +0800

    Minor fixes

--- Code playground/freeplay.c
+++ Code playground/freeplay.c
@@ -2,32 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
-void main()
-{
-    int number;
+int main() {
+    int i, sum = 0;
 
-    float temp, sqrt;
-
-    printf("Provide the number: \n");
-
-    scanf("%d", &number);
-
-    // store the half of the given number e.g from 256 => 128
-    sqrt = number / 2;
-    temp = 0;
-
-    // Iterate until sqrt is different of temp, that is updated on the loop
-    while(sqrt != temp){
-        // initially 0, is updated with the initial value of 128
-        // (on second iteration = 65)
-        // and so on
-        temp = sqrt;
-
-        // Then, replace values (256 / 128 + 128 ) / 2 = 65
-        // (on second iteration 34.46923076923077)
-        // and so on
-        sqrt = ( number/temp + temp) / 2;
+    for (i = 1; i <= 100; i++) {
+        sum = sum + i;
     }
-
-    printf("The square root of '%d' is '%f'", number, sqrt);
+    printf("%d", sum);
+    return 0;
 }
