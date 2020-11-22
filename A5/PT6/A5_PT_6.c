87b19d4aa174f3080e595088d22d825e247408fe
itztaylorau <itztaylorau@gmail.com>
2020-11-22 16:49:09 +0800

    Added support for A5_PT_6

--- A5/PT6/A5_PT_6.c
+++ A5/PT6/A5_PT_6.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main() {
 
-    class letter;
+	char input;
+    int continueflag = 1;
 
-    do
-
-}
+    do {
+        printf("Continue <Y/N>: ");
+        scanf("%c", &input);
+        if((input == 'Y') || (input == 'N')){
+            continueflag = 0;
+            printf("You have made a valid response.\n");
+        }else{
+            printf("Invalid response! Please try again\n");
+            fflush(stdin);
+        }} while (continueflag);
+    return 0;}
