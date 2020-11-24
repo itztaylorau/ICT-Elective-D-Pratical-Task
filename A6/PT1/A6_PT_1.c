c49fa42430ef4de4f88dec63edc35fa07414216a
itztaylorau <itztaylorau@gmail.com>
2020-11-24 11:15:23 +0800

    Code playground content upload

--- A6/PT1/A6_PT_1.c
+++ A6/PT1/A6_PT_1.c
@@ -5,6 +5,21 @@
 int alpha, beta, charlie;
 float root1, root2;
 
+void calRoots(){
+    root1 = (-beta + (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
+    root2 = (-beta - (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
+    findRoots();
+}
+
+void findRoots(){
+    if(root1 < 0.0 || root2 < 0.0){
+        printf("Root 1 = %.3f", root1);
+        printf("\nRoot 2 = %.3f", root2);
+    }else{
+        printf("No real roots");
+    }
+}
+
 int main() {
 
     printf("A: ");
@@ -14,15 +29,8 @@ int main() {
     printf("C: ");
     scanf("%d", &charlie);
 
-    root1 = (-beta + (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
+    calRoots();
 
-    root2 = (-beta - (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
 
-    if(root1 < 0.0 || root2 < 0.0){
-        printf("Root 1 = %.3f", root1);
-        printf("\nRoot 2 = %.3f", root2);
-    }else{
-        printf("No real roots");
-    }
     return 0;
 }
---
+++ Code playground/stars.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gen(int n){
+    if (n == 1){
+        printf("\n");
+    }else{
+        grn(n-1);
+        for (i=0; i < n; i++)
+    }
+    }
+}
+
+void main() {
+    gen(5);
+}
