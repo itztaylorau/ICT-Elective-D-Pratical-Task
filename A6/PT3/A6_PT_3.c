ea64a5fb13454bb6f907ddcdbdc08094ab91a58f
Sero Hui <itztaylorau@gmail.com>
2020-12-05 15:20:40 +0800

    Code changed due to wrong format

--- A6/PT3/A6_PT_3.c
+++ A6/PT3/A6_PT_3.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+int alpha, beta, charlie;
 float root1, root2;
 
-void calRoots(int alpha, int beta, int charlie){
-    root1 = (-beta + (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
-    root2 = (-beta - (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
+void calRoots(int *alpha, int *beta, int *charlie){
+    root1 = (-*beta + (sqrt((*beta * *beta) - 4 * *alpha * *charlie))) / (2 * *alpha);
+    root2 = (-*beta - (sqrt((*beta * *beta) - 4 * *alpha * *charlie))) / (2 * *alpha);
     return root1, root2;
 }
 
 void findRoots(){
+    calRoots(&alpha, &beta, &charlie);
     if(root1 < 0.0 || root2 < 0.0){
         printf("\nRoot 1 = %.3f", root1);
         printf("\nRoot 2 = %.3f", root2);
@@ -20,14 +22,12 @@ void findRoots(){
 }
 
 int main() {
-    int alpha, beta, charlie;
     printf("A: ");
     scanf("%d", &alpha);
     printf("B: ");
     scanf("%d", &beta);
     printf("C: ");
     scanf("%d", &charlie);
-    calRoots(alpha, beta, charlie);
-    findRoots(root1, root2);
+    findRoots();
     return 0;
 }
