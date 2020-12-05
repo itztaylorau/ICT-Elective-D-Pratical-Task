648527458690b291b8f2d83b715e4fc444d12bbd
itztaylorau <itztaylorau@gmail.com>
2020-12-05 14:42:22 +0800

    Fixed the problem caused by passing arguement error

--- A6/PT2/A6_PT_2.c
+++ A6/PT2/A6_PT_2.c
@@ -6,21 +6,17 @@ int alpha, beta, charlie;
 float root1, root2;
 
 void calRoots(int alpha, int beta, int charlie){
-    printf("%d, %d, %d\n", alpha, beta, charlie);
     root1 = (-beta + (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
     root2 = (-beta - (sqrt((beta * beta) - 4 * alpha * charlie))) / (2 * alpha);
-    printf("%f, %f\n", root1, root2);
-    findRoots(root1, root2);
+    return root1, root2;
 }
 
-void findRoots(float root1, float root2){
-    printf("%f, %f\n", root1, root2);
+void findRoots(){
+    calRoots(alpha, beta, charlie);
     if(root1 < 0.0 || root2 < 0.0){
-        printf("Root 1 = %.3f", root1);
+        printf("\nRoot 1 = %.3f", root1);
         printf("\nRoot 2 = %.3f", root2);
     }else{
-        printf("Root 1 = %.3f", root1);
-        printf("\nRoot 2 = %.3f\n", root2);
         printf("No real roots");
     }
 }
@@ -32,7 +28,6 @@ int main() {
     scanf("%d", &beta);
     printf("C: ");
     scanf("%d", &charlie);
-    printf("%d, %d, %d\n", alpha, beta, charlie);
-    calRoots(alpha, beta, charlie);
+    findRoots();
     return 0;
 }
