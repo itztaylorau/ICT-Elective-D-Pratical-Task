f11a17229cab97e6c40dd2944eb9befb1b5787d5
Sero Hui <itztaylorau@gmail.com>
2020-12-10 11:41:39 +0800

    New chapter Ch A8

--- Code playground/freeplay.c
+++ Code playground/freeplay.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int num[21];
-int count, i;
-
-void main()
-{
-    num[1]=1;
-    num[2]=1;
-    printf("Number of fibonacci numbers required  (maximum 20)");
-    scanf("%d",&count);
-    for(i=3;i<=count;i++)
-        num[i]=num[i-1]+num[i-2];
-
-    for(i=1;i<=count;i++){
-        printf("%d\t",num[i]);
-        if(i>1)
-            printf("%.4f",(float) num[i]/num[i-1]);
-        printf("\n");
+void main() {
+    char str[10] = "Peter 1";
+    int i;
+    for(i = 0; i < 10; i++){
+        printf("%c(%d) ------ %d\n", str[i], &str[i]);
     }
+    printf("\n%d\n", str);
+    return 0;
 }
