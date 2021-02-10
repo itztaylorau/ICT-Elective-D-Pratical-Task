477232d5c95f95db5fd1cbe774ab0cb80586173b
Sero Hui <itztaylorau@gmail.com>
2021-02-10 17:02:01 +0800

    Fixing A9_PT_1

--- A9/Partical Tasks/A9_PT_1.c
+++ A9/Partical Tasks/A9_PT_1.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 
-const char* convert_score(int score){
-    char *string_score[3];
-    itoa(score, string_score, 10);
-    printf("X %s", string_score);
-    return string_score;
-}
-
 int main() {
-    int i;
-    char *name[5] = {"Peter", "Mary", "John", "Bob", "Kathy"} , conduct[5] = {'B', 'C', 'A', 'C', 'B'};
+    int i = 0, j = 0, k = 0;
+    char *name[5] = {"Peter", "Mary", "John", "Bob", "Kathy"} , conduct[5] = {'B', 'C', 'A', 'C', 'B'}, *string_score[5][3], add_zero[2];
     int student_number[5] = {1001, 1002, 1003, 1004, 1005}, score[5] = {80, 0, 7, 100, 49};
     for(i = 0; i < 5; ++i){
+
+
+
             printf("Student name: %s\t", name[i]);
             printf("Student number: %d\t", student_number[i]);
             printf("Student score: %d\t", score[i]);
-            printf("%s\t", convert_score(90));
+            for(j = 0; j < 2; ++j){
+             itoa(score[i], string_score[i], 10);
+                         printf("Student score: %d\t", string_score[i]);
+            }
+
             printf("Student conduct: %c\n", conduct[i]);
             printf("%s\t%d\n", name[i], student_number[i]);
     }
