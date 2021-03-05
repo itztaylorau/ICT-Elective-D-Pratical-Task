610fbc2a93a1733a49be44420eef7f50fdd7d173
itztaylorau <itztaylorau@gmail.com>
2021-03-05 12:03:58 +0800

    Finally finished A10_PT_2

--- A10/Practical Task/A10_PT_2.c
+++ A10/Practical Task/A10_PT_2.c
@@ -8,7 +8,7 @@
 int main() {
     char *city[SIZE] = {"Amsterdam", "Auckland", "Bangkok", "Jakarta", "London", "Manila", "New York", "Osaka", "Paris", "Singapore", "Sydney", "Tokyo", "Vancouver", "Zurich"};
     char user_query[30];
-    int top, middle, last, query_stats = FALSE, pricing[SIZE] = {6500, 6200, 2100, 2375, 4985, 1450, 8300, 4250, 5100, 2215, 5400, 4600, 6000, 7500};
+    int top, middle, last, location, query_stats = FALSE, pricing[SIZE] = {6500, 6200, 2100, 2375, 4985, 1450, 8300, 4250, 5100, 2215, 5400, 4600, 6000, 7500};
 
     printf("Enter city name: ");
     scanf("%s", user_query);
@@ -18,11 +18,19 @@ int main() {
 
     while(top <= last && query_stats == FALSE){
         middle = (top + last) / 2;
-        printf("%s!", city[middle]);
-        printf("%d", strcmp(user_query, city[middle]);
         if(strcmp(user_query, city[middle]) == 0){
-            printf("Fare is $%d", pricing[middle]);
+            location = middle;
             query_stats = TRUE;
+        }else if(strcmp(user_query, city[middle]) == -1){
+            last = middle - 1;
+        }else{
+            top = middle + 1;
         }
+    }
+
+    if(query_stats){
+        printf("Fare is $%d\n", pricing[location]);
+    }else{
+        printf("%s is not in the list.", user_query);
     }
 }
