b4fe1fa9217b4fc6f9ff7dc6e1c68c84e4ae3be9
itztaylorau <itztaylorau@gmail.com>
2021-03-18 12:45:32 +0800

    Added some program
    + A9_PT_1.c (Finally finished after a month)
    + A9_PT_2.c (Still need some time lol)

--- A9/Partical Tasks/A9_PT_1.c
+++ A9/Partical Tasks/A9_PT_1.c
@@ -2,22 +2,34 @@
 #include <stdlib.h>
 #include <strings.h>
 #define arraySize 5
-char toString(int integerScore){
+char tmp[3];
+const char* toString(int integerScore){
     char stringScore[5];
     itoa(integerScore, stringScore, 10);
-    if(integerScore > 0 && integerScore < 10){
-        strcat("00", stringScore);
-        printf("%s", stringScore);
+    if(integerScore > -1 && integerScore < 10){
+        strcpy(tmp, "00");
+        strcat(tmp, stringScore);
+        return tmp;
     }else if(integerScore > 9 && integerScore < 100){
-        strcat("0", stringScore);
+        strcpy(tmp, "0");
+        strcat(tmp, stringScore);
+        return tmp;
+    }else{
+        strcpy(tmp, "");
+        strcat(tmp, stringScore);
+        return tmp;
     }
 }
 
 int main(){
     char *name[5] = {"Peter", "Mary", "John", "Bob", "Kathy"}, conduct[5] = {'B', 'C', 'A', 'C', 'B'};
     int i, studentNumber[5] = {1001, 1002, 1003, 1004, 1005}, score[5] = {80, 0, 7, 100, 49};
+    FILE *outfile;
+    outfile = fopen("assets\\ParticalTask1_output.txt", "w");
 
     for(i = 0; i < arraySize; ++i){
-        printf("%s\t%d%s%c\n", name[i], studentNumber[i], toString(score[i]), conduct[i]);
+        fprintf(outfile, "%s\t%d%s%c\n", name[i], studentNumber[i], toString(score[i]), conduct[i]);
     }
+    fclose(outfile);
+    return 0;
 }
---
+++ A9/Partical Tasks/A9_PT_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int i;
+    FILE *infile;
+    infile = fopen("assets\\ParticalTask2_source.txt", "r");
+    for(i = 0; i > )
+    fclose;
+}
