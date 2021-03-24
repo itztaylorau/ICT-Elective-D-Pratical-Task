aacac7149fbb438dd3e23694659afc58bea73e29
itztaylorau <itztaylorau@gmail.com>
2021-03-24 10:15:11 +0800

    Update

---
+++ A9/Partical Tasks/A9_PT_3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#define arraySize 5
+char tmp[3];
+const char* toString(int integerScore){
+    char stringScore[5];
+    itoa(integerScore, stringScore, 10);
+    if(integerScore > -1 && integerScore < 10){
+        strcpy(tmp, "00");
+        strcat(tmp, stringScore);
+        return tmp;
+    }else if(integerScore > 9 && integerScore < 100){
+        strcpy(tmp, "0");
+        strcat(tmp, stringScore);
+        return tmp;
+    }else{
+        strcpy(tmp, "");
+        strcat(tmp, stringScore);
+        return tmp;
+    }
+}
+
+int main(){
+    char *name[5] = {"Peter", "Mary", "John", "Bob", "Kathy"}, conduct[5] = {'B', 'C', 'A', 'C', 'B'};
+    int i, studentNumber[5] = {1001, 1002, 1003, 1004, 1005}, score[5] = {80, 0, 7, 100, 49};
+    FILE *outfile;
+    outfile = fopen("assets\\ParticalTask3_output.txt", "a");
+
+    for(i = 0; i < arraySize; ++i){
+        fprintf(outfile, "%s\t%d%s%c\n", name[i], studentNumber[i], toString(score[i]), conduct[i]);
+    }
+
+    fprintf(outfile, "%s\t%d%s%c\n", "David", 1012, toString(53), 'D');
+
+    fclose(outfile);
+    return 0;
+}
