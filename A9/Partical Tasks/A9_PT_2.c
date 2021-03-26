64bd56f979ed943e5ae70ccf0e65896c5e2cc19b
itztaylorau <itztaylorau@gmail.com>
2021-03-26 12:15:11 +0800

    Updated A9_PT_2.c

--- A9/Partical Tasks/A9_PT_2.c
+++ A9/Partical Tasks/A9_PT_2.c
@@ -1,26 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
-<<<<<<< Updated upstream
-=======
-#define fruit 5
-int accumulatedSum(char lineString[10]){
-    int i, j, digitSection;
-    char tmpStackString[11];
-    for(i = 0; i <= strlen(lineString); ++i){
-        if(isdigit(lineString[i]) || lineString[i] == '.'){
-            for(j = i; j <= strlen(lineString); ++j){
-                tmpStackString[j] = lineString[i];
-            }
-        }
-    }
-
-}
-
->>>>>>> Stashed changes
-int main(){
-    FILE *inFile;
-    inFile = fopen("assets\\ParticalTask2_source.txt", "r");
-
-    fclose();
-}
+#defin
