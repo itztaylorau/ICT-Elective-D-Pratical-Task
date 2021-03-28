b92d985e5653fa5861097dd281f54dc851fa85c4
Sero Hui <itztaylorau@gmail.com>
2021-03-28 20:01:11 +0800

    Updated A9_PT_2.c

--- A9/Partical Tasks/A9_PT_2.c
+++ A9/Partical Tasks/A9_PT_2.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
-#defin
+char storedResult[5][30];
+int i;
+void takePrice(char *inputData){
+    int j, k;
+    char priceStack[5][10];
+    for(j = 0; j <= strlen(inputData); ++j){
+        if(isdigit(inputData[j])){
+                for(k = 0; k < 10; ++k){
+                   priceStack[i][k] = inputData[j];
+                }
+
+        }
+    }
+    printf("AAA %s\n", priceStack[i]);
+}
+int main(){
+    FILE *inFile;
+    inFile = fopen("assets\\ParticalTask2_source.txt", "r");
+    char readResult[20];
+    while(fgets(readResult, 20, inFile) != NULL){
+        printf("%s", readResult);
+        strcpy(storedResult[i], readResult);
+        takePrice(storedResult[i]);
+        ++i;
+    }
+    fclose(inFile);
+    return 0;
+}
