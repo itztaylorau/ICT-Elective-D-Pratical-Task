a979dd25db088c91d93c48be936f1ee457f7f9bf
itztaylorau <itztaylorau@gmail.com>
2021-05-07 12:15:09 +0800

    idk

---
+++ A11/Practical Task/A11_PT_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#define stackSize 3
+int stackPointer;
+void initialStack(){
+ int i;
+ stackPointer = -1;
+ for(i = 0; i < stackPointer; ++i){
+
+ }
+}
+int main(){
+    initialStack();
+}
