a6e80cc0366d56dfb6c1ddaf7605bc19a206d8a6
Sero Hui <itztaylorau@gmail.com>
2021-05-09 17:45:30 +0800

    Final patch for A10_PT_1

--- A10/Practical Task/A10_PT_1.c
+++ A10/Practical Task/A10_PT_1.c
@@ -3,26 +3,26 @@
 #define FALSE 0
 #define TRUE 1
 
-int main() {
-	int flight_numbers[7] = {101, 135, 250, 272, 338, 479, 647}, i = 0, user_query, query_stats = FALSE;
+int main() {  
+	int flightNumbers[7] = {101, 135, 250, 272, 338, 479, 647}, i = 0, userQuery, queryStats = FALSE;
 
 	printf("Enter flight no.: ");
-	scanf("%d", &user_query);
+	scanf("%d", &userQuery);
 
-	while(i < SIZE && !query_stats){
-		if(user_query == flight_numbers[i] && query_stats != TRUE){
+	while(i < SIZE && !queryStats){
+		if(userQuery == flightNumbers[i] && queryStats != TRUE){
 			printf("Pass %d\n", i + 1);
-			printf("%d is found in location no. %d\n", user_query, i + 1);
-			query_stats = TRUE;
+			printf("%d is found in location no. %d\n", userQuery, i + 1);
+			queryStats = TRUE;
 		}else{
 			printf("Pass %d\n", i + 1);
-			query_stats = FALSE;
+			queryStats = FALSE;
 			++i;
 		}
 	}
 
-	if(query_stats == FALSE){
-		printf("%d is not found in the list.\n", user_query);
+	if(queryStats == FALSE){
+		printf("%d is not found in the list.\n", userQuery);
 	}
 
 	return 0;
