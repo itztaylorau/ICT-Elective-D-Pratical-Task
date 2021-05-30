73939ed04c50233372f156298cdf0864522e3d3d
Sero Hui <itztaylorau@gmail.com>
2021-05-30 16:51:36 +0800

    A11 changes

--- A11/Practical Task/A11_PT_2.c
+++ A11/Practical Task/A11_PT_2.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
-#define stackSize 3
-int stackPointer;
-void initialStack(){
- int i;
- stackPointer = -1;
- for(i = 0; i < stackPointer; ++i){
+#include <string.h>
+#define queueSize 4
+char timeQueue[queueSize][5], citizenQueue[queueSize][30];
+int head = 0, tail = 0;
 
- }
+/* Start of Maintainence module */
+void mtem() {
+  int i;
+  for(i = 0; i < queueSize; ++i){
+    printf("%d timeQueue: %s\n", i, timeQueue[i]);
+    printf("Head: %d, Tail: %d\n", head, tail);
+  }
+}/*End of Maintainence module */
+
+void displayQueue(){
+  int i;
+  for(i = 0; i < queueSize; ++i){
+    printf("%s\t%s\n", timeQueue[i], citizenQueue[i]);
+  }
 }
-int main(){
-    initialStack();
+void initQueue(){
+  int i;
+  for(i = 0; i < queueSize; ++i){
+      strcpy(timeQueue[i], "");
+      printf(timeQueue);
+      strcpy(citizenQueue[i], "");
+  }
+  mtem();
+  //displayQueue();
+}
+const char queue(char time[5], char citizen[30]){
+  if(tail >= queueSize){
+    printf("The queue is full.\n");
+  }else{
+    strcpy(timeQueue[tail], time);
+          printf("\nAA %s\n", timeQueue);
+    strcpy(citizenQueue[tail], citizen);
+    tail += 1;
+    mtem();
+    //displayQueue();
+
+  }
+
+}
+/*void dequeue(const char citizen){
+  int i, cmpResult;
+  for(i = 0; i < queueSize; ++i){
+    cmpResult = strcmp(citizen, citizenQueue[i]);
+    if(cmpResult == 0){
+      strcpy(citizenQueue[i], "");
+      strcpy(timeQueue[i], "");
+    }
+  }
+}*/
+int main() {
+  initQueue();
+  queue("10:00", "Ho Alex");
+        printf("\nTail is %d\n", tail);
+  queue("10:15", "Wong Mary");
+  //dequeue("Ho Alex");
+  //queue("10:30", "Lee Nicole");
+  //dequeue("Wong Mary");
+  //queue("10:45", "Au Stuart");
+  //queue("11:00", "Cheng Michelle");
+  //dequeue("Lee Nicole");
+  //`dequeue("Au Stuart");
+  return 0;
 }
